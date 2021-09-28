c0746db5e8c5a6178b4f5aaf5f504910e49c8ba0
MarceloSantonja <Marcelo.santonja@iesdoctorbalmis.com>
2021-09-28 19:01:57 +0200

    Agregar archivos de proyecto.

---
+++ UD2/adivinaNumero/adivinaNumero/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+using System.Windows;
+
+
+namespace AdivinaNumero
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int numeroAleatorio;
+        public MainWindow()
+        {
+            InitializeComponent();
+            CreaNumeroAleatorio();
+        }
+        public void CreaNumeroAleatorio()
+        {
+            Random semilla = new Random();
+            numeroAleatorio = semilla.Next(0, 101);
+        }
+
+
+
+        private void ComprobarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (int.TryParse(CajaNumerosTextBox.Text, out int numero))
+            {
+                if (numeroAleatorio == numero)
+                {
+                    RespuestaTextBlock.Text = "Has acertado";
+                }
+                else if (numeroAleatorio < numero)
+                {
+                    RespuestaTextBlock.Text = "Te has pasado";
+                }
+                else
+                {
+                    RespuestaTextBlock.Text = "Te has quedado corto";
+
+                }
+            }
+            else
+                RespuestaTextBlock.Text = "Eso no es un número";
+
+
+        }
+
+        private void ReiniciarButton_Click(object sender, RoutedEventArgs e)
+        {
+            CreaNumeroAleatorio();
+            RespuestaTextBlock.Text = "";
+        }
+    }
+
+
+}
