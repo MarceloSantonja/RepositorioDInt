6291eab4902ce9fda1cb11534623c3add0a1745d
MarceloSantonja <Marcelo.santonja@iesdoctorbalmis.com>
2021-09-28 19:38:34 +0200

    juntando Repositorios

---
+++ UD2/CalculadoraBasica/CalculadoraBasica/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace CalculadoraBasica
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int operando1;
+        private int operando2;
+ 
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CalcularButton_Click(object sender, RoutedEventArgs e)
+        {
+            bool numerosCorrectos = int.TryParse(Operando1TextBox.Text, out operando1) && int.TryParse(Operando2TextBox.Text, out operando2);
+            if (numerosCorrectos)
+            {
+                switch (OperadorTextBox.Text)
+                {
+                    case "*":
+                        ResultadoTextBox.Text = (operando1 * operando2).ToString();
+                        break;
+                    case "+":
+                        ResultadoTextBox.Text = (operando1 + operando2).ToString();
+                        break;
+                    case "-":
+                        ResultadoTextBox.Text = (operando1 - operando2).ToString();
+                        break;
+                    case "/":
+                        ResultadoTextBox.Text = (operando1 / operando2).ToString();
+                        break;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Se ha producido un error.Revise los operandos");
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (OperadorTextBox.Text == "*" || OperadorTextBox.Text == "+" || OperadorTextBox.Text == "-" || OperadorTextBox.Text == "/") { CalcularButton.IsEnabled = true; }
+            else
+            {
+                CalcularButton.IsEnabled = false;
+            }
+        }
+
+        private void LimpiarButton_Click(object sender, RoutedEventArgs e)
+        {
+            Operando1TextBox.Clear();
+            Operando2TextBox.Clear();
+            OperadorTextBox.Clear();
+            ResultadoTextBox.Clear();
+        }
+    }
+}
---
+++ UD2/CaracteresLimitados/CaracteresLimitados/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace CaracteresLimitados
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextoEscritoTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+            int tamaño = TextoEscritoTextBox.Text.Length;
+            MuestraContadorTextBlock.Text = tamaño + "/140";
+            if (tamaño >= 140) {
+                TextoEscritoTextBox.IsReadOnly = true;
+            
+            }
+
+        }
+
+
+
+
+    }
+}
