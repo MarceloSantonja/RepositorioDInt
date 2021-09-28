d2c9d8ad018927356f3a14606f5d8cd09d4e191b
MarceloSantonja <Marcelo.santonja@iesdoctorbalmis.com>
2021-09-28 21:00:16 +0200

    Ejercicio Texto con Formato

---
+++ UD2/TextoConFormato/TextoConFormato/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TextoConFormato
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void EscribeTextBox_TextChanged(object sender, TextChangedEventArgs e) => SalidaTextBlock.Text = EscribeTextBox.Text;
+
+        private void NegritaCheckBox_Checked(object sender, RoutedEventArgs e) => SalidaTextBlock.FontWeight = FontWeights.Bold;
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e) => SalidaTextBlock.FontStyle = FontStyles.Italic;
+
+        private void Azul_Checked(object sender, RoutedEventArgs e) => SalidaTextBlock.Foreground = Brushes.Blue;
+
+        private void Rojo_Checked(object sender, RoutedEventArgs e) => SalidaTextBlock.Foreground = Brushes.Red;
+
+        private void Verde_Checked(object sender, RoutedEventArgs e) => SalidaTextBlock.Foreground = Brushes.Green;
+    }
+}
