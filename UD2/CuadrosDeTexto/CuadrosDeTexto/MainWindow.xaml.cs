dbe80acce4c0345ad6cdbce3c7784fc568136c6a
MarceloSantonja <marcelo.santonja@iesdoctorbalmis.com>
2021-10-01 21:41:12 +0200

    ejercicio cuadros de texto

--- UD2/CuadrosDeTexto/CuadrosDeTexto/MainWindow.xaml.cs
+++ UD2/CuadrosDeTexto/CuadrosDeTexto/MainWindow.xaml.cs
@@ -24,5 +24,22 @@ namespace CuadrosDeTexto
         {
             InitializeComponent();
         }
+
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            TextBox caja = (TextBox)sender;
+            string[] datos = caja.Tag.ToString().Split('\\');
+            TextBlock ayuda =  (TextBlock)FindName(datos[0]);
+            if (e.Key == Key.F1)
+                ayuda.Text = ayuda.Text == "" ? datos[1]:"";
+        }
+
+        private void EdadTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            ErrorEdadtextBlock.Foreground = Brushes.Red;
+            if (e.Key == Key.F2)
+                ErrorEdadtextBlock.Text = int.TryParse(EdadTextBox.Text, out int edad) ? "" : "Edad incorrecta";
+
+        }
     }
 }
