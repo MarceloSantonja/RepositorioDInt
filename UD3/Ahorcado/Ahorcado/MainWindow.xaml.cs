0915aed27fb0de0339e8cea631682834c077390c
MarceloSantonja <marcelo.santonja@iesdoctorbalmis.com>
2021-10-22 12:54:49 +0200

    aaa

--- UD3/Ahorcado/Ahorcado/MainWindow.xaml.cs
+++ UD3/Ahorcado/Ahorcado/MainWindow.xaml.cs
@@ -31,32 +31,86 @@ namespace Ahorcado
             palabras.Add("caligrafia");
             palabras.Add("atardecer");
             palabras.Add("paisaje");
+            String letras = "ABCDEFGHIJKLMNÑOPQRSTUVWYZ";
+            List<Char> letrasList = letras.ToCharArray().ToList();
 
+            int contadorLetra = 0;
+            for (int fila = 0; fila < 3; fila++)
+            {
 
+                for (int columna = 0; columna < 7; columna++)
+                {
 
+                    TextBlock letra = new TextBlock
+                    {
+                        Text = letrasList[contadorLetra++].ToString()
+                    };
+                    Viewbox vista = new Viewbox
+                    {
 
+                        Child = letra
+                    };
+                    Button boton = new Button
+                    {
+                        Content = vista
 
+                    };
 
+                    this.TecladoGrid.Children.Add(boton);
+                    Grid.SetRow(boton, fila);
+                    Grid.SetColumn(boton, columna);
+
+                    
+
+                }
+            }
+            
 
 
         }
 
         private void NuevaPartidaButton_Click(object sender, RoutedEventArgs e)
         {
+            
             Random r = new Random();
-            int posicionAleatoria =r.Next(0, palabras.Count + 1);
+            int posicionAleatoria =r.Next(0, palabras.Count );
 
             String palabra = palabras[posicionAleatoria];
 
             char[] letras = palabra.ToCharArray();
 
-
-            TextBlock letra = new TextBlock
+            foreach (char i in letras)
             {
-                FontSize = 32,
-                
 
-            };
+                TextBlock letra = new TextBlock
+                {
+                    FontSize =60,
+                    FontWeight = FontWeights.Bold,
+                    Text = i.ToString(),
+                    //Visibility = Visibility.Hidden,
+                    Tag = i,
+                    Margin = new Thickness(10)
+                };
+                Border bordeLetra = new Border
+                {
+                    Height = 30,
+                    Padding = new Thickness(10),
+                    Margin = new Thickness(10),
+                    BorderThickness = new Thickness(0, 0, 0, 5),
+                    BorderBrush = Brushes.Black,
+                    Child = letra
+                };
+
+                this.PanelPalabra.Children.Add(bordeLetra);
+
+
+
+
+            }           
+            
+
+
+            
 
         }
     }
