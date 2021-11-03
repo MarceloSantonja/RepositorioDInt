84c5066be838a1c8e7539aea081be89effe3bff4
MarceloSantonja <marcelo.santonja@iesdoctorbalmis.com>
2021-11-03 20:36:56 +0100

    mememaker

---
+++ UD4/MemeMaker/MemeMaker/BordeConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MemeMaker
+{
+    class BordeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            
+            
+           return ((Boolean)value)? 3 : 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !((Boolean)value) ? 0 : 3;
+        }
+    }
+}
