315aab2af9e9b223fff74caf25af8a1743e44378
MarceloSantonja <marcelo.santonja@iesdoctorbalmis.com>
2021-11-08 08:14:50 +0100

    asssa

--- UD4/A2Superheroe/A2Superheroe/Properties/AssemblyInfo.cs
+++ UD4/A2Superheroe/A2Superheroe/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("superhéroes")]
+[assembly: AssemblyTitle("A2Superheroe")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("superhéroes")]
+[assembly: AssemblyProduct("A2Superheroe")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- UD4/A2Superheroe/A2Superheroe/Properties/Resources.Designer.cs
+++ UD4/A2Superheroe/A2Superheroe/Properties/Resources.Designer.cs
@@ -9,7 +9,7 @@
 //------------------------------------------------------------------------------
 
 
-namespace superhéroes.Properties
+namespace A2Superheroe.Properties
 {
     /// <summary>
     ///   Clase de recurso fuertemente tipado para buscar cadenas traducidas, etc.
@@ -43,7 +43,7 @@ namespace superhéroes.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("superhéroes.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("A2Superheroe.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
