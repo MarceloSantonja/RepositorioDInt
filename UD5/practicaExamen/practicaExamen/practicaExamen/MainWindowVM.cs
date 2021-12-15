945f4db9ebb378e44320eb9a18c67dddca2f7984
MarceloSantonja <marcelo.santonja@iesdoctorbalmis.com>
2021-12-15 23:41:34 +0100

    aaaa

---
+++ UD5/practicaExamen/practicaExamen/practicaExamen/MainWindowVM.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practicaExamen
+{
+    class MainWindowVM : ObservableObject
+    {
+
+        private PrediccionDia prediccionSelecionada;
+
+        private ObservableCollection<PrediccionDia> predicciones;
+
+        internal PrediccionDia PrediccionSelecionada { get => prediccionSelecionada; set => SetProperty(ref prediccionSelecionada, value); }
+        internal ObservableCollection<PrediccionDia> Predicciones { get => predicciones; set => SetProperty(ref predicciones, value); }
+
+        public MainWindowVM()
+        {
+            Predicciones = PrediccionDia.ObtenerDatos();
+        }
+
+
+    }
+}
