7a6a3606da55332ec01a7b83bbcc9b7eafbbdc39
MarceloSantonja <marcelo.santonja@iesdoctorbalmis.com>
2022-01-13 14:56:45 +0100

    aa

---
+++ UD6/WpfApp10/WpfApp10/ServicioNavegacion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WpfApp10
+{
+    class ServicioNavegacion
+    {
+        public void AbrirVentanaHija()
+        {
+            VentanaHija nueva = new VentanaHija();
+            nueva.ShowDialog();
+        }
+
+        internal UserControl AbrirUC2()
+        {
+            return new UserControl2();
+        }
+
+        internal UserControl AbrirUC1()
+        {
+            return new UserControl1();
+        }
+    }
+}
