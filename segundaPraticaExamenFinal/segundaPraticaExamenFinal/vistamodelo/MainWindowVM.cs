f3505d00c9d6542f829d4b4d75a712220bf43da4
MarceloSantonja <marcelo.santonja@iesdoctorbalmis.com>
2022-02-16 17:57:53 +0100

    aa

---
+++ segundaPraticaExamenFinal/segundaPraticaExamenFinal/vistamodelo/MainWindowVM.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using segundaPraticaExamenFinal.modelo;
+using segundaPraticaExamenFinal.Servicios;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace segundaPraticaExamenFinal.vistamodelo
+{
+    class MainWindowVM: ObservableObject
+    {
+        public ServicioNavegacion Navegacion { get;}
+        public UserControl UCCargado { get; }
+        public RelayCommand InfoCommand { get; }
+
+        private Componente componenteSeleccionado;
+
+        public Componente ComponenteSeleccionado
+        {
+            get { return componenteSeleccionado; }
+            set { SetProperty(ref componenteSeleccionado, value); }
+        }
+
+        private ObservableCollection<Componente> componentes;
+
+        public ObservableCollection<Componente> Componentes
+        {
+            get { return componentes; }
+            set { SetProperty(ref componentes, value); }
+        }
+
+        public MainWindowVM()
+        {
+            Componentes = Componente.GetSamples();
+            Navegacion = new ServicioNavegacion();
+            InfoCommand = new RelayCommand(AbrirInfo);
+        }
+
+        private void AbrirInfo()
+        {
+            Navegacion.AbrirVentanaInfo();
+        }
+    }
+}
